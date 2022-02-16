0251e288933a8e66adc9b44716c52045486fc80d
unknown <you@example.com>
2022-02-16 08:31:49 +0700

    nop bai

---
+++ module2/ss1_introduction_to_java/thuc_hanh/su_dung_toan_tu/src/OpreratorExp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class OpreratorExp {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter wight: ");
+        float wight=scanner.nextFloat();
+        System.out.println("Enter hight");
+        float hight=scanner.nextFloat();
+        float area=wight*hight;
+        System.out.println(area);
+
+    }
+}
